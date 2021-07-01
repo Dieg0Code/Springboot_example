c1129e47089c3c955d9b513e6aea0b400bdd919d
Diego Obando <diegoobando20@gmail.com>
2021-07-01 05:21:26 -0400

    Student Class added

--- src/main/java/com/dieg0Code/springbootex/SpringbootexApplication.java
+++ src/main/java/com/dieg0Code/springbootex/SpringbootexApplication.java
@@ -1,10 +1,14 @@
 package com.dieg0Code.springbootex;
 
+import com.dieg0Code.springbootex.student.Student;
+import org.apache.tomcat.jni.Local;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +20,17 @@ public class SpringbootexApplication {
     }
 
     @GetMapping
-    public List<String> hello() {
-        return List.of("Hello", "World");
+    public List<Student> hello() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Diego",
+                        "dieg0.code@email.com",
+                        LocalDate.of(1996, Month.FEBRUARY, 2),
+                        25
+
+                )
+        );
     }
 
 }
